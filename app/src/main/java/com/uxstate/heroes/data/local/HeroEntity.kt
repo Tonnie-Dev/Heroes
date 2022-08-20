a3c06b56e71a12e173bd3bd0266dc0cb315ec496
Tonnie <vontonnie@gmail.com>
2022-08-20 08:39:10 +0300

    Add Constants object in the util package

---
+++ app/src/main/java/com/uxstate/heroes/data/local/HeroEntity.kt
@@ -0,0 +1,4 @@
+package com.uxstate.heroes.data.local
+
+class HeroEntity {
+}
---
+++ app/src/main/java/com/uxstate/heroes/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.uxstate.heroes.util
+
+object Constants {
+
+    const val HERO_DATABASE_TABLE = "hero_table"
+}
