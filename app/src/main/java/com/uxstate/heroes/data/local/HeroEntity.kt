41c5181907ed5b57dd25fc30f6129ddaf7e102a4
Tonnie <vontonnie@gmail.com>
2022-08-20 08:51:31 +0300

    Add HeroEntity class

--- app/src/main/java/com/uxstate/heroes/data/local/HeroEntity.kt
+++ app/src/main/java/com/uxstate/heroes/data/local/HeroEntity.kt
@@ -1,4 +1,17 @@
 package com.uxstate.heroes.data.local
 
-class HeroEntity {
-}
+import androidx.room.Entity
+import com.uxstate.heroes.util.Constants.HERO_DATABASE_TABLE
+
+@Entity(tableName = HERO_DATABASE_TABLE)
+data class HeroEntity ( val id: Int,
+                        val name: String,
+                        val image: String,
+                        val about: String,
+                        val rating: Double,
+                        val power: Int,
+                        val month: String,
+                        val day: String,
+                        val family: List<String>,
+                        val abilities: List<String>,
+                        val natureTypes: List<String>)
