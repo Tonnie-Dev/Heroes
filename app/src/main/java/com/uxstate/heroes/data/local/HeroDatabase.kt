4067875f8824b32343d4e07f8fa85d7850e35cca
Tonnie <vontonnie@gmail.com>
2022-08-21 18:02:53 +0300

    Expose daos in the HeroDatabase

--- app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
+++ app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
@@ -3,11 +3,13 @@ package com.uxstate.heroes.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.uxstate.heroes.data.local.dao.HeroDao
+import com.uxstate.heroes.data.local.dao.HeroRemoteKeyDao
 import com.uxstate.heroes.data.local.entity.HeroEntity
 import com.uxstate.heroes.data.local.entity.HeroRemoteKeyEntity
 
 @Database(entities = [HeroEntity::class, HeroRemoteKeyEntity::class], version = 1)
 abstract class HeroDatabase:RoomDatabase() {
 
-    abstract val dao: HeroDao
+    abstract val heroDao: HeroDao
+    abstract val heroRemoteKeyDao:HeroRemoteKeyDao
 }
