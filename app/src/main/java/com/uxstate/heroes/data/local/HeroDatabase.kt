5372a42067a5828f44cb62d4284c880185222e5c
Tonnie <vontonnie@gmail.com>
2022-08-21 20:25:41 +0300

    Annotate HeroDatabase with @TypeConverters

--- app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
+++ app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
@@ -2,12 +2,14 @@ package com.uxstate.heroes.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.uxstate.heroes.data.local.dao.HeroDao
 import com.uxstate.heroes.data.local.dao.HeroRemoteKeyDao
 import com.uxstate.heroes.data.local.entity.HeroEntity
 import com.uxstate.heroes.data.local.entity.HeroRemoteKeyEntity
 
 @Database(entities = [HeroEntity::class, HeroRemoteKeyEntity::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class HeroDatabase:RoomDatabase() {
 
     abstract val heroDao: HeroDao
