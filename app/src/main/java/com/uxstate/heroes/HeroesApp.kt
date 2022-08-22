b43617dea1218df5381b4d18d7c4f5cbe91243d0
Tonnie <vontonnie@gmail.com>
2022-08-22 06:58:17 +0300

    Register HeroesApp application class in the manifest

--- app/src/main/java/com/uxstate/heroes/HeroesApp.kt
+++ app/src/main/java/com/uxstate/heroes/HeroesApp.kt
@@ -1,8 +1,11 @@
 package com.uxstate.heroes
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+
+@HiltAndroidApp
 class HeroesApp:Application() {
 
     override fun onCreate() {
