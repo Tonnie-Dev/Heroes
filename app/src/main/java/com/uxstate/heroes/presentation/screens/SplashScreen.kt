aa6ac8b0594f1fce013963b3774d1781e34a4957
Tonnie <vontonnie@gmail.com>
2022-08-22 07:45:04 +0300

    Add Splash composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/SplashScreen.kt
@@ -1,9 +1,42 @@
 package com.uxstate.heroes.presentation.screens
 
-import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import com.uxstate.heroes.R
+import com.uxstate.heroes.presentation.ui.theme.Purple500
+import com.uxstate.heroes.presentation.ui.theme.Purple700
 
 @Composable
 fun SplashScreen() {
-    
+
+}
+
+@Composable
+fun Splash() {
+
+    Box(
+            modifier = Modifier
+                    .background(
+                            brush = Brush.verticalGradient(
+                                    listOf(
+                                            Purple700,
+                                            Purple500
+                                    )
+                            )
+                    )
+                    .fillMaxSize(),
+            
+            contentAlignment = Alignment.Center
+    ){
+        
+        Image(painter = painterResource(id = R.drawable.ic_logo), contentDescription = )
+        
+    }
 }
