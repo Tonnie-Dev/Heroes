5501d0dd944ecbeaf727269365207d759e452c36
Tonnie <vontonnie@gmail.com>
2022-08-22 07:22:29 +0300

    Add provideDatabase() di function

--- app/src/main/java/com/uxstate/heroes/di/AppModule.kt
+++ app/src/main/java/com/uxstate/heroes/di/AppModule.kt
@@ -1,13 +1,27 @@
 package com.uxstate.heroes.di
 
+import android.app.Application
+import androidx.room.Room
+import com.uxstate.heroes.data.local.HeroDatabase
+import com.uxstate.heroes.util.Constants.HERO_DATABASE
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
 
+    fun provideDatabase(app: Application): HeroDatabase {
+
+
+        return Room.databaseBuilder(app, HeroDatabase::class.java, HERO_DATABASE)
+                .build()
+    }
 
 }
