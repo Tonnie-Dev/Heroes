ecefba620ade53a78762e0ca7372b33e5d73eaf0
Tonnie <vontonnie@gmail.com>
2022-08-23 08:39:07 +0300

    Add HorizontalPager composable inside welcome screen

--- app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeScreen.kt
@@ -5,19 +5,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.uxstate.heroes.presentation.ui.theme.welcomeScreenBackgroundColor
+import com.uxstate.heroes.util.Constants
+
+@OptIn(ExperimentalPagerApi::class)
 @RootNavGraph(start = true)
 @Destination
 @Composable
 fun WelcomeScreen() {
 
+    val state = rememberPagerState()
     val pages =
         listOf(OnboardingPage.FirstPage, OnboardingPage.SecondPage, OnboardingPage.ThirdPage)
 
-    Column(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colors.welcomeScreenBackgroundColor)) {
+    Column(
+            modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colors.welcomeScreenBackgroundColor)
+    ) {
 
+        HorizontalPager(
+                count = Constants.ON_BOARDING_PAGE_COUNT,
+                state = state,
+                verticalAlignment = Alignment.Top
+        ) {
+
+        }
     }
 }
