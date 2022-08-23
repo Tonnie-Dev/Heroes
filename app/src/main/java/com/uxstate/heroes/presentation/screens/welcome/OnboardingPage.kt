85b0269864a1c2e2ab28a9944e97353508e6b0bd
Tonnie <vontonnie@gmail.com>
2022-08-23 08:16:06 +0300

    Add extension variables on MaterialTheme.colors variable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/OnboardingPage.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/OnboardingPage.kt
@@ -8,5 +8,5 @@ sealed class OnboardingPage(@DrawableRes image: Int, title: String, description:
 
     object FirstPage : OnboardingPage(R.drawable.greetings,"Greetings", "Are you a Boruto Fan? Because if you are we have great news for you!")
     object SecondPage : OnboardingPage(R.drawable.explore, "Explore", "Find your favorite heroes and learn some of the things that you didn't know about")
-    object ThirdPage : OnboardingPage(R.drawable.power, "Power", "Find your favorite heroes and learn some of the things that you didn't know about")
+    object ThirdPage : OnboardingPage(R.drawable.power, "Power", "Check out your hero's power and see how much strong they are compared to others")
 }
--- app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,8 +1,23 @@
 package com.uxstate.heroes.presentation.screens.welcome
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.uxstate.heroes.presentation.ui.theme.welcomeScreenBackgroundColor
+@RootNavGraph(start = true)
+@Destination
 @Composable
 fun WelcomeScreen() {
 
+    val pages =
+        listOf(OnboardingPage.FirstPage, OnboardingPage.SecondPage, OnboardingPage.ThirdPage)
+
+    Column(modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colors.welcomeScreenBackgroundColor)) {
+
+    }
 }
