96760a095f1d2b5e85a52392714128f00a5b8c95
Tonnie <vontonnie@gmail.com>
2022-08-24 07:44:47 +0300

    Add readnBoardingState DataStoreOperations interface function

--- app/src/main/java/com/uxstate/heroes/domain/repository/DataStoreOperations.kt
+++ app/src/main/java/com/uxstate/heroes/domain/repository/DataStoreOperations.kt
@@ -1,6 +1,10 @@
 package com.uxstate.heroes.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
+
 interface DataStoreOperations {
 
     suspend fun saveOnBoardingState(isCompleted:Boolean)
+    fun readOnBoardingState(): Flow<Boolean>
 }
