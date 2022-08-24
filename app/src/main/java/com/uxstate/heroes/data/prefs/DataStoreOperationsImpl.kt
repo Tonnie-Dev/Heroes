0a5f3a2099699bf474e88709f038d0c6e12a77c4
Tonnie <vontonnie@gmail.com>
2022-08-24 08:00:31 +0300

    Add dataStore extension function from Context class

--- app/src/main/java/com/uxstate/heroes/data/prefs/DataStoreOperationsImpl.kt
+++ app/src/main/java/com/uxstate/heroes/data/prefs/DataStoreOperationsImpl.kt
@@ -1,8 +1,16 @@
 package com.uxstate.heroes.data.prefs
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.uxstate.heroes.domain.repository.DataStoreOperations
+import com.uxstate.heroes.util.Constants
 import kotlinx.coroutines.flow.Flow
 
+//extension variable from DataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.PREFERENCES_NAME)
 class DataStoreOperationsImpl:DataStoreOperations {
     override suspend fun saveOnBoardingState(isCompleted: Boolean) {
         TODO("Not yet implemented")
