4e70f15f11a4bb20b32c7b792c85003341067f48
Tonnie <vontonnie@gmail.com>
2022-08-25 08:41:32 +0300

    Add RepositoryModule abstract class in the di package

---
+++ app/src/main/java/com/uxstate/heroes/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.uxstate.heroes.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    //REPOSITORY
+    @Binds //@Binds used for 1-to-1 interface-implementation mapping
+    @Singleton
+
+    /*The abstract function takes only a single parameter which
+    is the interface implementation and the return type is the
+    interface implemented by the given parameter object.*/
+
+    abstract fun provideData
+
+}
