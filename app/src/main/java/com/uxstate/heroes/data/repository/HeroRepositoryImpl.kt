10333fa7c0c0c6fdbafa0e9e06ba75cd40e4f0ec
Tonnie <vontonnie@gmail.com>
2022-08-26 07:41:57 +0300

    Implement HeroRepositoryImpl functions

--- app/src/main/java/com/uxstate/heroes/data/repository/HeroRepositoryImpl.kt
+++ app/src/main/java/com/uxstate/heroes/data/repository/HeroRepositoryImpl.kt
@@ -1,6 +1,20 @@
 package com.uxstate.heroes.data.repository
 
+import com.uxstate.heroes.data.prefs.DataStoreOperations
+import com.uxstate.heroes.domain.repository.HeroRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class HeroRepositoryImpl @Inject constructor() {
+//always depend on abstraction and not concretions
+class HeroRepositoryImpl @Inject constructor(private val dataStoreOperations: DataStoreOperations) :
+    HeroRepository {
+    override suspend fun saveOnboardingState(isCompleted: Boolean) {
+        dataStoreOperations.saveOnBoardingState(isCompleted = isCompleted)
+    }
+
+    override fun readOnboardingStatus(): Flow<Boolean> {
+        return dataStoreOperations.readOnBoardingState()
+    }
+
+
 }
