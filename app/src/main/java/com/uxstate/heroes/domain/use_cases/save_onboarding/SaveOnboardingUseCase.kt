dc831e3a8a5fff79bc98ec90e60740f6e837f406
Tonnie <vontonnie@gmail.com>
2022-08-26 07:57:10 +0300

    Add invoke function to SaveOnboardingUseCase class

--- app/src/main/java/com/uxstate/heroes/domain/use_cases/save_onboarding/SaveOnboardingUseCase.kt
+++ app/src/main/java/com/uxstate/heroes/domain/use_cases/save_onboarding/SaveOnboardingUseCase.kt
@@ -3,4 +3,10 @@ package com.uxstate.heroes.domain.use_cases.save_onboarding
 import com.uxstate.heroes.domain.repository.HeroRepository
 
 class SaveOnboardingUseCase(private val repository: HeroRepository) {
+
+
+    suspend operator fun invoke(isCompleted:Boolean){
+
+        repository.saveOnboardingState(isCompleted)
+    }
 }
