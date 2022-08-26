a97a9c579eb3325fea46c24241739dffc35f5f7a
Tonnie <vontonnie@gmail.com>
2022-08-26 08:21:08 +0300

    Add saveOnboardingState()

--- app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/WelcomeViewModel.kt
@@ -1,11 +1,26 @@
 package com.uxstate.heroes.presentation.screens.welcome
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.heroes.domain.use_cases.UseCaseWrapper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(private val useCaseWrapper: UseCaseWrapper) : ViewModel() {
+
+
+  fun  saveOnboardingState(isCompleted:Boolean) {
+
+      viewModelScope.launch (Dispatchers.IO){
+
+          useCaseWrapper.saveOnboardingUseCase(isCompleted = isCompleted)
+
+      }
+
+
+  }
 }
