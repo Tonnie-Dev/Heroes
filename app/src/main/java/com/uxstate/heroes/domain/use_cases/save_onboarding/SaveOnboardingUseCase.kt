1ed1ee9fe3c0b3facb16e640b30f748429e13b2e
Tonnie <vontonnie@gmail.com>
2022-08-26 07:49:37 +0300

    Add SaveOnboardingUseCase

---
+++ app/src/main/java/com/uxstate/heroes/domain/use_cases/save_onboarding/SaveOnboardingUseCase.kt
@@ -0,0 +1,6 @@
+package com.uxstate.heroes.domain.use_cases.save_onboarding
+
+import com.uxstate.heroes.domain.repository.HeroRepository
+
+class SaveOnboardingUseCase(private val repository: HeroRepository) {
+}
