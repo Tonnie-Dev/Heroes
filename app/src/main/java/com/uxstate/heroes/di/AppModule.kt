e7c3c7d78eede7c0d41346c0aec76acfd21bb539
Tonnie <vontonnie@gmail.com>
2022-08-26 08:05:23 +0300

    Add provideUseCaseWrapperClass di function

--- app/src/main/java/com/uxstate/heroes/di/AppModule.kt
+++ app/src/main/java/com/uxstate/heroes/di/AppModule.kt
@@ -3,6 +3,10 @@ package com.uxstate.heroes.di
 import android.app.Application
 import androidx.room.Room
 import com.uxstate.heroes.data.local.HeroDatabase
+import com.uxstate.heroes.domain.repository.HeroRepository
+import com.uxstate.heroes.domain.use_cases.UseCaseWrapper
+import com.uxstate.heroes.domain.use_cases.read_onboarding.ReadOnboardingUseCase
+import com.uxstate.heroes.domain.use_cases.save_onboarding.SaveOnboardingUseCase
 import com.uxstate.heroes.util.Constants.HERO_DATABASE
 import dagger.Module
 import dagger.Provides
@@ -22,6 +26,18 @@ object AppModule {
 
         return Room.databaseBuilder(app, HeroDatabase::class.java, HERO_DATABASE)
                 .build()
+    }
+
+    @Provides
+    @Singleton
+
+    fun provideUseCaseWrapperClass(repository: HeroRepository): UseCaseWrapper {
+
+
+        return UseCaseWrapper(
+                readOnboardingUseCase = ReadOnboardingUseCase(repository),
+                saveOnboardingUseCase = SaveOnboardingUseCase(repository)
+        )
     }
 
 }
