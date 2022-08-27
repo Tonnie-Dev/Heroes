0751e80bc18565056d89cdf9bf55c5aaaf7df351
Tonnie <vontonnie@gmail.com>
2022-08-27 20:00:31 +0300

    Add IconButton to HomeTopBar

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/HomeTopBar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/HomeTopBar.kt
@@ -1,25 +1,37 @@
 package com.uxstate.heroes.presentation.screens.home.components
 
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.heroes.R
 import com.uxstate.heroes.presentation.ui.theme.topAppBarBackgroundColor
 import com.uxstate.heroes.presentation.ui.theme.topAppBarContentColor
 
 @Composable
-fun HomeTopBar() {
+fun HomeTopBar(onSearch: () -> Unit) {
 
-    TopAppBar(title = {Text(
-            text = stringResource(R.string.explore_label),
-            color = MaterialTheme.colors.topAppBarContentColor
-    )}, backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor, actions = { Icon(
-            imageVector = Icons.Default.Search,
-            contentDescription = stringResource(R.string.search_icon)
-    )})
+    TopAppBar(title = {
+        Text(
+                text = stringResource(R.string.explore_label),
+                color = MaterialTheme.colors.topAppBarContentColor
+        )
+    }, backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor, actions = {
+
+        IconButton(onClick = { onSearch() }) {
+            Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+            )
+        }
+    })
+}
+
+
+@Preview(name = "HomeTopBar")
+@Composable
+fun HomeTopBarPreview() {
+
 }
