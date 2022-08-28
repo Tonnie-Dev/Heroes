1355141a02efc3dae1ce98d4451437b9f996faf5
Tonnie <vontonnie@gmail.com>
2022-08-28 10:19:15 +0300

    -a
    Add companion object to HeroAPI interface

--- app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
+++ app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
@@ -1,15 +1,20 @@
 package com.uxstate.heroes.data.remote
 
-import com.uxstate.heroes.domain.model.ApiResponse
+import com.uxstate.heroes.data.remote.dto.ApiResponseDTO
+
 import com.uxstate.heroes.domain.model.Hero
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface HeroAPI {
-
-    @GET("/boruto/heroes/search")
-    fun searchHero(@Query("name") name: String):List<Hero>
-
     @GET("boruto/heroes")
-    fun getAllHeroes(@Query("page") page:Int):ApiResponse
+    suspend fun getAllHeroes(@Query("page") page:Int = 1):ApiResponseDTO
+    @GET("boruto/heroes/search")
+    suspend fun searchHeroes(@Query("name") name: String):ApiResponseDTO
+
+
+    companion object {
+
+        const val BASE_URL = "localhost:8080/"
+    }
 }
