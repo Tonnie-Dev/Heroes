f1b5555c000732a557745b4fe4af4b55c505bc46
Tonnie <vontonnie@gmail.com>
2022-08-28 09:42:42 +0300

    Add getAllHeroes() Hero API interface function

--- app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
+++ app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
@@ -1,5 +1,6 @@
 package com.uxstate.heroes.data.remote
 
+import com.uxstate.heroes.domain.model.ApiResponse
 import com.uxstate.heroes.domain.model.Hero
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,4 +9,7 @@ interface HeroAPI {
 
     @GET("/boruto/heroes/search")
     fun searchHero(@Query("name") name: String):List<Hero>
+
+    @GET("boruto/heroes")
+    fun getAllHeroes(@Query("page") page:Int):ApiResponse
 }
