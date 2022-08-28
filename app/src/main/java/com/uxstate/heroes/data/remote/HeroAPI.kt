5c7cd758fe3e8cee32db5afc7feaf8f93a17c508
Tonnie <vontonnie@gmail.com>
2022-08-28 09:36:59 +0300

    Add searchHero Hero API interface function

--- app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
+++ app/src/main/java/com/uxstate/heroes/data/remote/HeroAPI.kt
@@ -1,4 +1,11 @@
 package com.uxstate.heroes.data.remote
 
+import com.uxstate.heroes.domain.model.Hero
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface HeroAPI {
+
+    @GET("/boruto/heroes/search")
+    fun searchHero(@Query("name") name: String):List<Hero>
 }
