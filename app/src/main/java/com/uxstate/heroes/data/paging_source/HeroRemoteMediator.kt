3e0329a5c53e5cfe0866474681dd21b90f931a8c
Tonnie <vontonnie@gmail.com>
2022-08-31 17:54:51 +0300

    Inject data source dependencies into HeroRemoteMediator

--- app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
@@ -4,10 +4,13 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import com.uxstate.heroes.data.local.HeroDatabase
+import com.uxstate.heroes.data.remote.HeroAPI
 import com.uxstate.heroes.domain.model.Hero
+import javax.inject.Inject
 
 @OptIn(ExperimentalPagingApi::class)
-class HeroRemoteMediator: RemoteMediator<Int, Hero>() {
+class HeroRemoteMediator @Inject constructor(api:HeroAPI, database: HeroDatabase): RemoteMediator<Int, Hero>() {
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
         TODO("Not yet implemented")
     }
