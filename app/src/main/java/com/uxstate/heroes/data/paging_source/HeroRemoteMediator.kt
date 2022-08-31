ab346bff233a446d2943fb55e25acf0532fdb627
Tonnie <vontonnie@gmail.com>
2022-08-31 18:34:55 +0300

    add HeroRemoteKeys model class

--- app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
@@ -4,25 +4,69 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import com.uxstate.heroes.data.local.HeroDatabase
 import com.uxstate.heroes.data.remote.HeroAPI
 import com.uxstate.heroes.domain.model.Hero
 import javax.inject.Inject
 
+
+/*query is a simple string which define which data
+ retrieve from the backend server*/
 @OptIn(ExperimentalPagingApi::class)
 class HeroRemoteMediator @Inject constructor(
-    api: HeroAPI,
-    database: HeroDatabase
+    private val api: HeroAPI,
+   private val database: HeroDatabase
 ) : RemoteMediator<Int, Hero>() {
 
     //get daos
    private val heroDao = database.heroDao
    private val heroRemoteKeysDao = database.heroRemoteKeysDao
 
-
+/*responsible for updating the backing dataset and invalidating the paging source*/
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
-        TODO("Not yet implemented")
-    }
+       try {
 
+           val response = api.getAllHeroes()
+           //check the heroes list from api is not empty
+           if (response.heroes.isNotEmpty()){
+
+               //if not empty save items into database
+
+               database.withTransaction {
+
+                 /* LoadType.Refresh is triggered the 1st time app is installed
+                 * or when the data is invalidated*/
+
+
+                   if (loadType ==LoadType.REFRESH){
+
+                       //clear tables
+                       heroDao.deleteAllHeroes()
+                       heroRemoteKeysDao.deleteAllRemoteKeys()
+
+                   }
+
+                   val prevPage = response.prevPage
+                   val nextPage = response.nextPage
+
+                   //retrieve ids from heroes
+                   val keys = response.heroes.map {
+
+                       it.id
+
+
+                   }
+               }
+
+               }}
+               catch (e:Exception){
+
+                   return MediatorResult.Error(e)
+               }
+
+
+
+}
 
 }
---
+++ app/src/main/java/com/uxstate/heroes/domain/model/HeroRemoteKeys.kt
@@ -0,0 +1,5 @@
+package com.uxstate.heroes.domain.model
+
+data class HeroRemoteKeys(val id: Int,
+                          val prevPage: Int?,
+                          val nextKey: Int?)
