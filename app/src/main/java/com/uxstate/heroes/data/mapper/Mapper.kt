664c74579ea5d791a03977bd249ac986483b3daa
Tonnie <vontonnie@gmail.com>
2022-08-31 19:41:02 +0300

    Overried HeroRemoteMediator's load() function

--- app/src/main/java/com/uxstate/heroes/data/mapper/Mapper.kt
+++ app/src/main/java/com/uxstate/heroes/data/mapper/Mapper.kt
@@ -1,14 +1,34 @@
 package com.uxstate.heroes.data.mapper
 
+import com.uxstate.heroes.data.local.entity.HeroEntity
+import com.uxstate.heroes.data.local.entity.HeroRemoteKeysEntity
+import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.domain.model.HeroRemoteKeys
 
 //HeroRemoteKeys to HeroRemoteKeysEntity
 
-fun HeroRemoteKeys.toEntity(): HeroRemoteKeys {
+fun HeroRemoteKeys.toEntity(): HeroRemoteKeysEntity {
 
-    return HeroRemoteKeys(
+    return HeroRemoteKeysEntity(
             id = this.id,
             prevPage = this.prevPage,
             nextKey = this.nextKey
+    )
+}
+
+fun Hero.toEntity(): HeroEntity {
+
+    return HeroEntity(
+            id = this.id,
+            name = this.name,
+            image =this.image,
+            about = this.about,
+            rating = this.rating,
+            power = this.power,
+            month = this.month,
+            day = this.day,
+            family = this.family,
+            abilities = this.abilities,
+            natureTypes = this.natureTypes
     )
 }
--- app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
@@ -6,6 +6,7 @@ import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import androidx.room.withTransaction
 import com.uxstate.heroes.data.local.HeroDatabase
+import com.uxstate.heroes.data.mapper.toEntity
 import com.uxstate.heroes.data.remote.HeroAPI
 import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.domain.model.HeroRemoteKeys
@@ -26,9 +27,11 @@ class HeroRemoteMediator @Inject constructor(
 
     /*responsible for updating the backing dataset and invalidating the paging source*/
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
-        try {
+        return try {
 
+            //Make API response
             val response = api.getAllHeroes()
+
             //check the heroes list from api is not empty
             if (response.heroes.isNotEmpty()) {
 
@@ -37,17 +40,14 @@ class HeroRemoteMediator @Inject constructor(
                 database.withTransaction {
 
                     /* LoadType.Refresh is triggered the 1st time app is installed
-                    * or when the data is invalidated*/
-
-
+                   or when the data is invalidated*/
                     if (loadType == LoadType.REFRESH) {
-
                         //clear tables
                         heroDao.deleteAllHeroes()
                         heroRemoteKeysDao.deleteAllRemoteKeys()
-
                     }
 
+                    //extract pages values to enable saving the HeroRemoteKeys
                     val prevPage = response.prevPage
                     val nextPage = response.nextPage
 
@@ -58,16 +58,21 @@ class HeroRemoteMediator @Inject constructor(
                                 prevPage = prevPage,
                                 nextKey = nextPage
                         )
-
-
                     }
 
-                    //Add keys into database
-                    heroRemoteKeysDao.addAllKeys(keys)
+                    //populate keys into database
+                    heroRemoteKeysDao.addAllKeys(keys.map { it.toEntity() })
+
+                    //populate heroes into database from the server
+                    heroDao.addHeroes(response.heroes.map { it.toEntity() })
 
                 }
 
+
             }
+
+            //return success passing boolean to signal end of page
+            MediatorResult.Success(endOfPaginationReached = response.nextPage == null)
         } catch (e: Exception) {
 
             return MediatorResult.Error(e)
