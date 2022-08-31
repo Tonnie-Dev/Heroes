af881d9b50928aa7aa508f06b50eb3ea57535e86
Tonnie <vontonnie@gmail.com>
2022-08-31 17:47:17 +0300

    add RemoteMediator's load() function

--- app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/HeroRemoteMediator.kt
@@ -1,4 +1,14 @@
 package com.uxstate.heroes.data.paging_source
 
-class HeroRemoteMediator {
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.uxstate.heroes.domain.model.Hero
+
+@OptIn(ExperimentalPagingApi::class)
+class HeroRemoteMediator: RemoteMediator<Int, Hero>() {
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
+        TODO("Not yet implemented")
+    }
 }
