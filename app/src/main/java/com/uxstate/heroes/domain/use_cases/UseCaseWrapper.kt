6b6c5f773b8c74ae5068863d733fee5b638c4588
Tonnie <vontonnie@gmail.com>
2022-09-05 20:23:04 +0300

    Add getAllHeroesUseCase property to UseCaseWrapper

--- app/src/main/java/com/uxstate/heroes/domain/use_cases/UseCaseWrapper.kt
+++ app/src/main/java/com/uxstate/heroes/domain/use_cases/UseCaseWrapper.kt
@@ -1,9 +1,11 @@
 package com.uxstate.heroes.domain.use_cases
 
+import com.uxstate.heroes.domain.use_cases.get_heroes.GetAllHeroesUseCase
 import com.uxstate.heroes.domain.use_cases.read_onboarding.ReadOnboardingUseCase
 import com.uxstate.heroes.domain.use_cases.save_onboarding.SaveOnboardingUseCase
 
 data class UseCaseWrapper(
     val readOnboardingUseCase: ReadOnboardingUseCase,
     val saveOnboardingUseCase: SaveOnboardingUseCase
+    val getAllHeroesUseCase: GetAllHeroesUseCase
 )
