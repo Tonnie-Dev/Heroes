eeaeba200932017e5fb95702177d1b4a44e7e8ea
Tonnie <vontonnie@gmail.com>
2022-09-05 20:22:25 +0300

    Add invoke function to GetAllHeroesUseCase

--- app/src/main/java/com/uxstate/heroes/domain/use_cases/get_heroes/GetAllHeroesUseCase.kt
+++ app/src/main/java/com/uxstate/heroes/domain/use_cases/get_heroes/GetAllHeroesUseCase.kt
@@ -1,6 +1,17 @@
 package com.uxstate.heroes.domain.use_cases.get_heroes
 
+import androidx.paging.PagingData
+import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.domain.repository.HeroRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetAllHeroesUseCase(private val repository: HeroRepository) {
+
+
+    operator fun invoke(): Flow<PagingData<Hero>>{
+
+
+        return repository.getAllHeroes()
+    }
 }
