1bdc2a7b9a53c2098aacfdd31f29ab3efe425a55
Tonnie <vontonnie@gmail.com>
2022-09-05 20:17:03 +0300

    Add GetAllHeroesUseCase class

---
+++ app/src/main/java/com/uxstate/heroes/domain/use_cases/get_heroes/GetAllHeroesUseCase.kt
@@ -0,0 +1,6 @@
+package com.uxstate.heroes.domain.use_cases.get_heroes
+
+import com.uxstate.heroes.domain.repository.HeroRepository
+
+class GetAllHeroesUseCase(private val repository: HeroRepository) {
+}
