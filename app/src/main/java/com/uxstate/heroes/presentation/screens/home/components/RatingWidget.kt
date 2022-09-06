250f1f6dbe1829d8e9ec2fd28e9c8aef376a7d09
Tonnie <vontonnie@gmail.com>
2022-09-06 08:57:42 +0300

    Add RatingWidget composable

---
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidget.kt
@@ -0,0 +1,9 @@
+package com.uxstate.heroes.presentation.screens.home.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RatingWidget(modifier: Modifier, rating:Double) {
+
+}
