90b735bd4782d38c10c1ffb02fe8b27e4820cfa0
Tonnie <vontonnie@gmail.com>
2022-09-06 19:24:44 +0300

    Add path variables to RatingWidget composables

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidget.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidget.kt
@@ -1,9 +1,24 @@
 package com.uxstate.heroes.presentation.screens.home.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.PathParser
+import androidx.compose.ui.res.stringResource
+import com.uxstate.heroes.R
 
 @Composable
 fun RatingWidget(modifier: Modifier, rating:Double) {
+  
+  val starPathString = stringResource(id = R.string.star_path)
 
+    /*Parses the path string to create a collection of PathNode instances*/
+    val starPath = remember{ PathParser().parsePathString(pathData = starPathString).toPath()}
+
+    //holds the path bounds
+
+    val starPathBounds = remember {
+        starPath.getBounds()
+
+    }
 }
