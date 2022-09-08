72f379fe34e8db2cacfeae69ccda9c1388feaf4a
Tonnie <vontonnie@gmail.com>
2022-09-08 07:27:10 +0300

    Add CardRowSample StackOverflow question

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/HalfFilledStar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/HalfFilledStar.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.uxstate.heroes.presentation.ui.theme.LightGray
 import com.uxstate.heroes.presentation.ui.theme.starColor
 
 @Composable
@@ -21,24 +24,43 @@ fun HalfFilledStar(
 ) {
 
 
-
-
     //Canvas
     Canvas(modifier = Modifier.size(24.dp), onDraw = {
 
-       scale(scaleFactor){
-           val size = this.size
+        scale(scaleFactor) {
+            val canvasSize = this.size
 
-           val x = (size.width/2f) - (pathBounds.width/2f)
-           val y = (size.height/2f) - (pathBounds.height/2f)
+            val x = (canvasSize.width / 2f) - (pathBounds.width / 2f)
+            val y = (canvasSize.height / 2f) - (pathBounds.height / 2f)
 
-           translate(left = x, top = y){
+            translate(left = x, top = y) {
 
-               drawPath(path = starPath, color = starColor)
-           }
+                drawPath(
+                        path = starPath,
+                        color = LightGray.copy(alpha = 0.5f)
+                )
+            }
+            //accepts path for bottom layer
+            clipPath(starPath) {
 
-       }
 
+                //draw scope
+
+                drawRect(
+                        color = starColor,
+
+                        size = Size(
+
+                                //Max.Dimens The greater of the magnitudes of the
+                                // width and the height of this rectangle.
+                                width = pathBounds.maxDimension / 1.7f,
+                                height = pathBounds.maxDimension*scaleFactor
+                        )
+                )
+            }
+
+
+        }
 
 
     })
