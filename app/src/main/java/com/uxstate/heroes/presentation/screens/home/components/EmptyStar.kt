56e600673a80900ab1fc09ca6dfb3d67d38e89b2
Tonnie <vontonnie@gmail.com>
2022-09-14 08:26:47 +0300

    Draw Empty star inside EmptyStar composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/EmptyStar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/EmptyStar.kt
@@ -1,15 +1,40 @@
 package com.uxstate.heroes.presentation.screens.home.components
 
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun EmptyStar(starPathBounds: Rect, starPath:Path, scaleFactor:Float) {
 
+Canvas(modifier = Modifier.size(24.dp), onDraw = {
+    val width = starPathBounds.width
+    val height = starPathBounds.height
 
+
+    val x = (this.size.width)/2- width/2
+    val y = (this.size.height)/2 - height/2
+
+   scale(scale = scaleFactor){
+
+
+       translate(left = x , top = y){
+
+
+           drawPath(path = starPath, color = Color.LightGray.copy(alpha = 0.5f))
+       }
+   }
+
+})
 
 
 
@@ -20,5 +45,5 @@ fun EmptyStar(starPathBounds: Rect, starPath:Path, scaleFactor:Float) {
 @Composable
 fun EmptyStarPreview() {
 
-
+RatingWidget(modifier = Modifier, rating = 2.0)
 }
