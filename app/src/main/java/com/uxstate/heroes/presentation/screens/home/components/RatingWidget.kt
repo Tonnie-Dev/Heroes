ae4591978c3f42df1ae114cfdc710d1d6d685a25
Tonnie <vontonnie@gmail.com>
2022-09-14 19:16:24 +0300

    Declare variables for calculateStars()

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidget.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidget.kt
@@ -1,6 +1,8 @@
 package com.uxstate.heroes.presentation.screens.home.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.PathParser
@@ -8,16 +10,19 @@ import androidx.compose.ui.res.stringResource
 import com.uxstate.heroes.R
 
 @Composable
-fun RatingWidget(modifier: Modifier, rating:Double, scaleFactor:Float= 3f) {
+fun RatingWidget(modifier: Modifier, rating: Double, scaleFactor: Float = 3f) {
 
-  val starPathString = stringResource(id = R.string.star_path)
+    val starPathString = stringResource(id = R.string.star_path)
 
     /*convert the string to to a Path/ PathNode instances*/
-    val starPath = remember{ PathParser().parsePathString(pathData = starPathString).toPath()}
+    val starPath = remember {
+        PathParser().parsePathString(pathData = starPathString)
+                .toPath()
+    }
 
 
-  //Compute the bounds of the control points of the path,
-  // and write the answer into bounds
+    //Compute the bounds of the control points of the path,
+    // and write the answer into bounds
     val starPathBounds = remember {
         starPath.getBounds()
 
@@ -33,8 +38,16 @@ fun RatingWidget(modifier: Modifier, rating:Double, scaleFactor:Float= 3f) {
 and an Int representing the stars to be drawn*/
 
 @Composable
-fun calculateStars(rating: Double): Map<String,Int> {
+fun calculateStars(rating: Double): Map<String, Int> {
 
+   /* cache only - prevent expensive re-calculation operation on
+    recomposition*/
+    val maxStars = remember { 5 }
 
+    /*cache, observe and trigger recomposition of any composable
+    reading the value*/
+    val filledStars by remember { mutableStateOf(0) }
+    val halfFilledStars by remember { mutableStateOf(0) }
+    val emptyStars by remember { mutableStateOf(0) }
 
 }
