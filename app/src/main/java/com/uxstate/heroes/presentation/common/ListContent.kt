21d7e152f862456ba9a868049f9249e6b32f5a1a
Tonnie <vontonnie@gmail.com>
2022-09-15 08:54:41 +0300

    Add parameters to ListContent composable function

--- app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
@@ -1,8 +1,11 @@
 package com.uxstate.heroes.presentation.common
 
 import androidx.compose.runtime.Composable
+import androidx.paging.compose.LazyPagingItems
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.heroes.domain.model.Hero
 
 @Composable
-fun ListContent() {
+fun ListContent(heroes:LazyPagingItems<Hero>, navigator: DestinationsNavigator) {
 
 }
--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/HomeScreen.kt
@@ -8,13 +8,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.presentation.common.ListContent
 import com.uxstate.heroes.presentation.screens.destinations.Destination
 import com.uxstate.heroes.presentation.screens.home.components.HomeTopBar
 
 @com.ramcosta.composedestinations.annotation.Destination
 @Composable
-fun HomeScreen( viewModel: HomeViewModel = hiltViewModel()) {
+fun HomeScreen( viewModel: HomeViewModel = hiltViewModel(), navigator: DestinationsNavigator) {
 
 
     val allHeroes = viewModel.getAllHeroes.collectAsLazyPagingItems()
@@ -22,7 +23,7 @@ fun HomeScreen( viewModel: HomeViewModel = hiltViewModel()) {
 
     Scaffold( topBar = { HomeTopBar {}}, content = { paddingValues ->
 
-        ListContent()
+        ListContent(heroes = allHeroes, navigator =navigator)
     })
 
 
