957793c5df3813d625d6af7e008ec7a1780625a0
Tonnie <vontonnie@gmail.com>
2022-09-16 08:11:09 +0300

    Add DetailsScreen composable

--- app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
@@ -1,16 +1,31 @@
 package com.uxstate.heroes.presentation.common
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.paging.compose.LazyPagingItems
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.domain.model.Hero
 
 @Composable
-fun ListContent(heroes:LazyPagingItems<Hero>, navigator: DestinationsNavigator) {
+fun ListContent(heroes: LazyPagingItems<Hero>, navigator: DestinationsNavigator) {
 
 }
 
 @Composable
 fun HeroItem(navigator: DestinationsNavigator, hero: Hero) {
+
+    Box(
+            modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(3f / 2f)
+                    .clickable {
+
+                    }) {
+
+    }
 
 }
---
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/details/DetailsScreen.kt
@@ -0,0 +1,8 @@
+package com.uxstate.heroes.presentation.screens.home.details
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DetailsScreen() {
+    
+}
