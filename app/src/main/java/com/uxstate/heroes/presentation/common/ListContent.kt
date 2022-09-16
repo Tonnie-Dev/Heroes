ba5b514edd584da090251d8d4d3f209d7728fe91
Tonnie <vontonnie@gmail.com>
2022-09-16 08:46:39 +0300

    Declare the imagePainter for the HeroItem

--- app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/common/ListContent.kt
@@ -1,15 +1,23 @@
 package com.uxstate.heroes.presentation.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.paging.compose.LazyPagingItems
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.presentation.screens.destinations.DetailsScreenDestination
+import com.uxstate.heroes.util.Constants.BASE_URL
 
 @Composable
 fun ListContent(heroes: LazyPagingItems<Hero>, navigator: DestinationsNavigator) {
@@ -19,6 +27,12 @@ fun ListContent(heroes: LazyPagingItems<Hero>, navigator: DestinationsNavigator)
 @Composable
 fun HeroItem(navigator: DestinationsNavigator, hero: Hero) {
 
+    val url = "$BASE_URL${hero.image}"
+    val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                    .data(url)
+    )
+
     Box(
             modifier = Modifier
                     .fillMaxWidth()
@@ -26,6 +40,16 @@ fun HeroItem(navigator: DestinationsNavigator, hero: Hero) {
                     .clickable {
                         navigator.navigate(DetailsScreenDestination)
                     }) {
+
+        Surface(shape = MaterialTheme.shapes.large) {
+
+            Image(
+                    painter = painter,
+                    contentDescription = hero.name,
+                    contentScale = ContentScale.Crop
+            )
+
+        }
 
     }
 
