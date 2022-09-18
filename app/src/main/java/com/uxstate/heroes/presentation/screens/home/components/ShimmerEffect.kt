6fb1945187ae7a43437a1a62cad498e6a320e101
Tonnie <vontonnie@gmail.com>
2022-09-18 10:51:41 +0300

    Add ShimmerPreviewLight

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/ShimmerEffect.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/components/ShimmerEffect.kt
@@ -1,8 +1,56 @@
 package com.uxstate.heroes.presentation.screens.home.components
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.uxstate.heroes.presentation.ui.theme.ShimmerDarkGray
+import com.uxstate.heroes.presentation.ui.theme.ShimmerLightGray
+import com.uxstate.heroes.presentation.ui.theme.ShimmerMediumGray
+import com.uxstate.heroes.util.LocalSpacing
 
 @Composable
 fun ShimmerEffect() {
-    
+
+}
+
+@Composable
+fun ShimmerItem() {
+
+    val spacing = LocalSpacing.current
+
+    Surface(
+            modifier = Modifier.fillMaxWidth(),
+            color = if (isSystemInDarkTheme()) Color.Black else ShimmerLightGray,
+            shape = RoundedCornerShape(spacing.spaceMedium)
+    ) {
+        Column(
+                modifier = Modifier.padding(spacing.spaceMedium),
+                verticalArrangement = Arrangement.Bottom
+        ) {
+
+
+            Surface(
+                    modifier = Modifier
+                            .fillMaxWidth(0.5f)
+                            .height(spacing.spaceLarge),
+                    color = if (isSystemInDarkTheme()) ShimmerDarkGray else ShimmerMediumGray,
+                    shape = RoundedCornerShape(spacing.spaceSmall)
+            ) {
+
+            }
+        }
+    }
+}
+
+@Preview(name = "ShimmerPreview - Light", showBackground = true)
+@Composable
+fun ShimmerPreviewLight() {
+
+    ShimmerItem()
+
 }
