bb1194d8f7180407f5ee1404350ea0c9737e860d
Tonnie <vontonnie@gmail.com>
2022-09-20 08:31:31 +0300

    Add EmptyState composable function  in the common package

---
+++ app/src/main/java/com/uxstate/heroes/presentation/common/EmptyScreen.kt
@@ -0,0 +1,9 @@
+package com.uxstate.heroes.presentation.common
+
+import androidx.compose.runtime.Composable
+import androidx.paging.LoadState
+
+@Composable
+fun EmptyScreen(error:LoadState.Error) {
+
+}
