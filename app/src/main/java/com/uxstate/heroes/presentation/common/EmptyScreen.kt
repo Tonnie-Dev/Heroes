7be4ffb592bf399d7c28a26a25504478db336e88
Tonnie <vontonnie@gmail.com>
2022-09-20 20:16:59 +0300

    Add Icon composable in the EmptyScreen composable

--- app/src/main/java/com/uxstate/heroes/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/common/EmptyScreen.kt
@@ -1,22 +1,28 @@
 package com.uxstate.heroes.presentation.common
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.paging.LoadState
 import com.uxstate.heroes.R
+import com.uxstate.heroes.util.LocalSpacing
 
 @Composable
 fun EmptyScreen(error: LoadState.Error) {
+
+    val spacing = LocalSpacing.current
 
     val message by remember {
         mutableStateOf(parseErrorMessage(error.toString()))
@@ -32,11 +38,13 @@ fun EmptyScreen(error: LoadState.Error) {
             verticalArrangement = Arrangement.Center
     ) {
 
+        //Icon
 
-        Image(
-                painterResource(id = icon),
-                contentDescription = message,
-                contentScale = ContentScale.Crop
+        Icon(
+                modifier = Modifier.size(spacing.spaceOneHundredFifty),
+                painter = painterResource(id = icon),
+                contentDescription = stringResource(R.string.network_error_icon),
+                tint = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
         )
     }
 
