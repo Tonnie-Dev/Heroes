76d19d13b0e3e77dd01f152351820ac314ab0c8c
Tonnie <vontonnie@gmail.com>
2022-09-20 08:48:50 +0300

    Add parseErrorMessage

--- app/src/main/java/com/uxstate/heroes/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/common/EmptyScreen.kt
@@ -1,9 +1,22 @@
 package com.uxstate.heroes.presentation.common
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.paging.LoadState
 
 @Composable
-fun EmptyScreen(error:LoadState.Error) {
+fun EmptyScreen(error: LoadState.Error) {
+
+    val message by remember{ mutableStateOf(parseErrorMessage(error.error)) }
+
+}
+
+fun parseErrorMessage(message: String): String {
+
+    return when {
+        message.contains("SocketTimeoutException") -> "Server Unavailable"
+        message.contains("ConnectionException") -> "Internet Unavailable"
+
+        else -> "Unknown Error"
+    }
 
 }
