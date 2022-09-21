978a1b659ee72fccf1f6fde9f850827981eca353
Tonnie <vontonnie@gmail.com>
2022-09-21 08:10:34 +0300

    Add SearchTopBar composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
@@ -1,8 +1,15 @@
 package com.uxstate.heroes.presentation.screens.splash.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun SearchTopBar() {
-    
+fun SearchTopBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
 }
