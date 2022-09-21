98c794be5a44742437b6d1c76d08717364f8b5d1
Tonnie <vontonnie@gmail.com>
2022-09-21 08:25:53 +0300

    Add SearchWidget preview functions

--- app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
@@ -1,5 +1,7 @@
 package com.uxstate.heroes.presentation.screens.splash.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,6 +12,7 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.heroes.R
 import com.uxstate.heroes.util.LocalSpacing
 
@@ -38,9 +41,10 @@ fun SearchWidget(
     Surface(modifier = modifier.padding(spacing.spaceSmall)) {
 
 
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row() {
 
             TextField(
+                    modifier = Modifier.fillMaxWidth(),
                     value = text,
                     onValueChange = onTextChange,
                     placeholder = { Text(text = "Search Here ...") },
@@ -54,10 +58,28 @@ fun SearchWidget(
                     },
                     trailingIcon = {
                         IconButton(onClick = onClose) {
-                            Icon(imageVector = Icons.Default.Clear, contentDescription =)
+                            Icon(imageVector = Icons.Default.Clear, contentDescription = "Close")
                         }
                     })
         }
     }
+
+}
+
+
+@Preview(name = "Light", uiMode = UI_MODE_NIGHT_NO, showSystemUi = true, showBackground = true)
+@Composable
+fun SearchWidgetPreviewLight() {
+
+    SearchWidget(text = "", onTextChange = {}, onSearch = {}, onClose = { })
+
+}
+
+
+@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES, showSystemUi = true, showBackground = true)
+@Composable
+fun SearchWidgetPreviewDark() {
+
+    SearchWidget(text = "", onTextChange = {}, onSearch = {}, onClose = { })
 
 }
