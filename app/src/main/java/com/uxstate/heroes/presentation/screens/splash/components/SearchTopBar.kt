9fdb42b7b78d222fc6cf818134985b8157187b9c
Tonnie <vontonnie@gmail.com>
2022-09-21 08:19:08 +0300

    Add AsearchWidget composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/splash/components/SearchTopBar.kt
@@ -1,7 +1,17 @@
 package com.uxstate.heroes.presentation.screens.splash.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.uxstate.heroes.R
+import com.uxstate.heroes.util.LocalSpacing
 
 @Composable
 fun SearchTopBar(
@@ -11,5 +21,43 @@ fun SearchTopBar(
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+}
+
+
+@Composable
+fun SearchWidget(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val spacing = LocalSpacing.current
+
+    Surface(modifier = modifier.padding(spacing.spaceSmall)) {
+
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+
+            TextField(
+                    value = text,
+                    onValueChange = onTextChange,
+                    placeholder = { Text(text = "Search Here ...") },
+                    leadingIcon = {
+                        Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = stringResource(
+                                        R.string.search_lable
+                                )
+                        )
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = onClose) {
+                            Icon(imageVector = Icons.Default.Clear, contentDescription =)
+                        }
+                    })
+        }
+    }
 
 }
