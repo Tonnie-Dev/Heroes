f56bab1c83fe5f1c528ebb8d60e3e5111c6178b9
Tonnie <vontonnie@gmail.com>
2022-09-22 08:31:40 +0300

    Add SearchQueries to ViewModel

--- app/src/main/java/com/uxstate/heroes/presentation/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/search/SearchViewModel.kt
@@ -1,5 +1,8 @@
 package com.uxstate.heroes.presentation.screens.search
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -8,5 +11,10 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchViewModel @Inject constructor() : ViewModel() {
 
+    var searchQuery by mutableStateOf("")
+    private set
+
+    var searchText by mutableStateOf("")
+    private set
 
 }
