5001dd71d956f5b01cf3bc4e22cf9d7a0c2764b7
Tonnie <vontonnie@gmail.com>
2022-09-23 08:10:52 +0300

    Add PagingSource override functions

--- app/src/main/java/com/uxstate/heroes/data/paging_source/SearchHeroesSource.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/SearchHeroesSource.kt
@@ -1,4 +1,18 @@
 package com.uxstate.heroes.data.paging_source
 
-class SearchHeroesSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.uxstate.heroes.domain.model.Hero
+
+
+/*We need to create our own PagingSource since we are not
+* using ROOM*/
+class SearchHeroesSource: PagingSource<Int, Hero>() {
+    override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Hero> {
+        TODO("Not yet implemented")
+    }
 }
