ae76bc1ca35c3d7172b60d7e3d166183b6361448
Tonnie <vontonnie@gmail.com>
2022-09-23 19:39:05 +0300

    Implement load() on SearchHeroesSource

--- app/src/main/java/com/uxstate/heroes/data/paging_source/SearchHeroesSource.kt
+++ app/src/main/java/com/uxstate/heroes/data/paging_source/SearchHeroesSource.kt
@@ -2,17 +2,41 @@ package com.uxstate.heroes.data.paging_source
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.uxstate.heroes.data.remote.HeroAPI
 import com.uxstate.heroes.domain.model.Hero
+import javax.inject.Inject
 
 
 /*We need to create our own PagingSource since we are not
 * using ROOM*/
-class SearchHeroesSource: PagingSource<Int, Hero>() {
+class SearchHeroesSource @Inject constructor(
+    private val api: HeroAPI,
+    private val query: String
+) : PagingSource<Int, Hero>() {
     override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
+
         TODO("Not yet implemented")
+
+
     }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Hero> {
-        TODO("Not yet implemented")
+        return  try {
+
+            val apiResponse = api.searchHeroes(query)
+            val heroes = apiResponse.heroes
+
+            if(heroes.isNotEmpty()){
+
+                LoadResult.Page(data = heroes, prevKey = apiResponse.prevPage, nextKey = apiResponse.nextPage)
+            }else {
+
+                LoadResult.Page(data = emptyList(), prevKey = null, nextKey = null)
+            }
+        }
+        catch (e:Exception){
+            LoadResult.Error(e)
+
+        }
     }
 }
