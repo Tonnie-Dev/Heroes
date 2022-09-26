b283313d765b749a881690f494e5815d080508cd
Tonnie <vontonnie@gmail.com>
2022-09-26 07:23:11 +0300

    Add InfoBox composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/DetailsScreen.kt
@@ -2,9 +2,10 @@ package com.uxstate.heroes.presentation.screens.details
 
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
-fun DetailsScreen() {
+fun DetailsScreen(navigator: DestinationsNavigator) {
 
 }
---
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/InfoBox.kt
@@ -0,0 +1,16 @@
+package com.uxstate.heroes.presentation.screens.details.components
+
+import android.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun InfoBox(
+    icon: Painter,
+    iconColor: Color,
+    bigText: String,
+    smallText: String,
+    textColor: Color
+) {
+
+}
