e995d7ceb0b36a8193fede384a0c415c4370a77a
Tonnie <vontonnie@gmail.com>
2022-09-28 08:35:01 +0300

    Add BottomSheetScaffold in DetailsContent

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/DetailsContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/DetailsContent.kt
@@ -1,10 +1,27 @@
 package com.uxstate.heroes.presentation.screens.details.components
 
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.domain.model.Hero
+import com.uxstate.heroes.util.LocalSpacing
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun DetailsContent(navigator: DestinationsNavigator, selectedHero:Hero) {
+fun DetailsContent(navigator: DestinationsNavigator, selectedHero: Hero) {
+
+    val spacing = LocalSpacing.current
+    val scaffoldState =
+        rememberBottomSheetScaffoldState(
+                bottomSheetState =
+                BottomSheetState(initialValue = BottomSheetValue.Expanded)
+        )
+    BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetPeekHeight = spacing.spaceOneHundredFifty,
+            sheetContent =
+    ) {
+
+    }
 
 }
