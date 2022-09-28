6cdc468775e36a04d085c3373d5a6c0a6ca3e7a7
Tonnie <vontonnie@gmail.com>
2022-09-28 08:38:50 +0300

    Add BottomSheetContent composable function

---
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BottomSheetContent.kt
@@ -0,0 +1,17 @@
+package com.uxstate.heroes.presentation.screens.details.components
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.uxstate.heroes.domain.model.Hero
+import com.uxstate.heroes.presentation.ui.theme.titleColor
+
+@Composable
+fun BottomSheetContent(
+    selectedHero: Hero,
+    infoBoxIconColor: Color = MaterialTheme.colors.primary,
+    sheetBackgroundColor: Color = MaterialTheme.colors.surface,
+    contentColor: Color = MaterialTheme.colors.titleColor
+) {
+
+}
