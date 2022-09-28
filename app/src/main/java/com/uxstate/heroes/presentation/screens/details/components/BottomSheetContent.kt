4370450e9888badcbd1f3bf23379c0ec29201802
Tonnie <vontonnie@gmail.com>
2022-09-28 20:49:25 +0300

    Add OrderedLists into BottomSheetContent composable function

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BottomSheetContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BottomSheetContent.kt
@@ -3,13 +3,14 @@ package com.uxstate.heroes.presentation.screens.details.components
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -105,19 +106,54 @@ fun BottomSheetContent(
 
         }
 
-        Text(modifier = Modifier.fillMaxWidth(),
+        Text(
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(spacing.spaceSmall),
                 text = stringResource(R.string.about_label),
                 color = contentColor,
                 fontSize = MaterialTheme.typography.subtitle1.fontSize,
                 fontWeight = FontWeight.Bold
         )
         //about text
-        Text(modifier = Modifier.fillMaxWidth(),
+        Text(
+                modifier = Modifier
+                        .alpha(ContentAlpha.medium)
+                        .padding(spacing.spaceSmall),
                 text = hero.about,
                 color = contentColor,
                 fontSize = MaterialTheme.typography.body1.fontSize,
                 maxLines = ABOUT_TEXT_MAX_LINES
         )
+
+
+        //Ordered List Row
+
+        Row(
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(spacing.spaceSmall),
+                horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            OrderedList(
+                    title = stringResource(id = R.string.family_label),
+                    items = hero.family,
+                    textColor = contentColor
+            )
+
+            OrderedList(
+                    title = stringResource(R.string.abilities_label),
+                    items = hero.abilities,
+                    textColor = contentColor
+            )
+
+
+            OrderedList(
+                    title = stringResource(R.string.nature_types),
+                    items = hero.natureTypes,
+                    textColor = contentColor
+            )
+        }
     }
 }
 
@@ -127,19 +163,30 @@ fun BottomSheetContent(
 fun BottomSheetContentLight() {
 
 
-    BottomSheetContent(hero = Hero(
-            id = 45,
-            name = "Tonnie",
-            image = "",
-            about = "Richest Man in Babylon",
-            rating = 5.0,
-            power = 97,
-            month = "January",
-            day = "6th",
-            family = listOf("Njiri", "Muchiri"),
-            abilities = listOf("Computing", "Scrabble"),
-            natureTypes = listOf("Trees", "Water")
-    )
+    BottomSheetContent(
+            hero = Hero(
+                    id = 45,
+                    name = "Tonnie",
+                    image = "",
+                    about = """                    
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                        
+                    """,
+                    rating = 5.0,
+                    power = 97,
+                    month = "January",
+                    day = "6th",
+                    family = listOf("Njiri", "Muchiri"),
+                    abilities = listOf("Computing", "Scrabble"),
+                    natureTypes = listOf("Trees", "Water")
+            )
     )
 
 }
@@ -149,26 +196,32 @@ fun BottomSheetContentLight() {
 fun BottomSheetContentDark() {
 
 
-        HeroesTheme() {
-            BottomSheetContent(hero = Hero(
-                    id = 45,
-                    name = "Tonnie",
-                    image = "",
-                    about = "Richest Man in Babylon",
-                    rating = 5.0,
-                    power = 97,
-                    month = "January",
-                    day = "6th",
-                    family = listOf("Njiri", "Muchiri"),
-                    abilities = listOf("Computing", "Scrabble"),
-                    natureTypes = listOf("Trees", "Water")
-            )
-            )
-        }
-
-
-
-
+    HeroesTheme() {
+        BottomSheetContent(
+                hero = Hero(
+                        id = 45,
+                        name = "Tonnie",
+                        image = "",
+                        about = """Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                        
+                    """.trimIndent(),
+                        rating = 5.0,
+                        power = 97,
+                        month = "January",
+                        day = "6th",
+                        family = listOf("Njiri", "Muchiri"),
+                        abilities = listOf("Computing", "Scrabble"),
+                        natureTypes = listOf("Trees", "Water")
+                )
+        )
+    }
 
 
 }
