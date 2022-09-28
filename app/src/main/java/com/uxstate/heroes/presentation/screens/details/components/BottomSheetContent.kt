b9ed714b912c25b491e4b1a230233fa8255396ed
Tonnie <vontonnie@gmail.com>
2022-09-28 08:54:40 +0300

    Add Logo Row to BottomSheet Content

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BottomSheetContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BottomSheetContent.kt
@@ -1,17 +1,67 @@
 package com.uxstate.heroes.presentation.screens.details.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.uxstate.heroes.R
 import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.presentation.ui.theme.titleColor
+import com.uxstate.heroes.util.LocalSpacing
 
 @Composable
 fun BottomSheetContent(
-    selectedHero: Hero,
+    hero: Hero,
     infoBoxIconColor: Color = MaterialTheme.colors.primary,
     sheetBackgroundColor: Color = MaterialTheme.colors.surface,
     contentColor: Color = MaterialTheme.colors.titleColor
 ) {
+    val spacing = LocalSpacing.current
 
+    Column(
+            modifier = Modifier
+                    .background(sheetBackgroundColor)
+                    .padding(spacing.spaceMedium)
+    ) {
+
+        //Logo Row
+        Row(
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = spacing.spaceMedium),
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            //log
+            Icon(
+                    painter = painterResource(id = R.drawable.ic_logo),
+                    modifier = Modifier
+                            .size(spacing.spaceLarge)
+                            .weight(2f),
+                    contentDescription = stringResource(
+                            id = R.string.app_logo
+                    ),
+                    tint = contentColor
+            )
+
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            //text
+
+            Text(
+                    text = hero.name,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(8f),
+                    color = contentColor,
+                    fontSize = MaterialTheme.typography.h4.fontSize
+            )
+        }
+    }
 }
