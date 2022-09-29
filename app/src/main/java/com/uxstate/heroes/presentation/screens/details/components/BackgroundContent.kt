71d3984b2025fd42a3f780cb8c4307bbef8ba724
Tonnie <vontonnie@gmail.com>
2022-09-29 20:13:26 +0300

    Add Background Imaged into BackgroundContent composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BackgroundContent.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/components/BackgroundContent.kt
@@ -1,8 +1,23 @@
 package com.uxstate.heroes.presentation.screens.details.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.uxstate.heroes.R
+import com.uxstate.heroes.util.Constants.BASE_URL
 
 @Composable
 fun BackgroundContent(
@@ -11,4 +26,29 @@ fun BackgroundContent(
     backgroundColor: Color = MaterialTheme.colors.surface,
     onCloseClicked: () -> Unit
 ) {
+
+
+    val imageUrl = "$BASE_URL$heroImage"
+    val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                    .data(imageUrl)
+                    .build()
+    )
+    Box(
+            modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+    ) {
+
+        Image(
+                painter = painter,
+                contentDescription = stringResource(R.string.hero_image),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(imageFraction)
+                        .align(Alignment.TopStart)
+        )
+
+    }
 }
