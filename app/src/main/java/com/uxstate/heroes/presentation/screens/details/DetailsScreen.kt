4d39592d3450dc16ec2061d792dc973d5d63351d
Tonnie <vontonnie@gmail.com>
2022-10-02 15:23:46 +0300

    Add LaunchBlock to the Details Screen

--- app/src/main/java/com/uxstate/heroes/presentation/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/details/DetailsScreen.kt
@@ -1,16 +1,63 @@
 package com.uxstate.heroes.presentation.screens.details
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.domain.model.Hero
 import com.uxstate.heroes.presentation.screens.details.components.DetailsContent
-import timber.log.Timber
+import com.uxstate.heroes.util.Constants.BASE_URL
+import com.uxstate.heroes.util.PaletteGenerator
+import kotlinx.coroutines.flow.collectLatest
 
 @Destination
 @Composable
-fun DetailsScreen(navigator: DestinationsNavigator, hero: Hero) {
+fun DetailsScreen(
+    navigator: DestinationsNavigator,
+    hero: Hero,
+    viewModel: DetailsViewModel = hiltViewModel()
+) {
 
-DetailsContent(navigator = navigator, hero = hero)
+    val colorPalette by viewModel.colorPalette
+
+    if (colorPalette.isNotEmpty()) {
+        DetailsContent(navigator = navigator, hero = hero)
+
+    } else {
+        viewModel.generateColorPalette()
+    }
+
+
+    val context = LocalContext.current
+
+
+    //doesn't re-trigger during recomposition
+    LaunchedEffect(key1 = true, block = {
+
+        viewModel.uiEvent.collectLatest { uiEvent ->
+
+            when (uiEvent) {
+
+                is UIEvent.GenerateColorPalette -> {
+
+                    //extract a bitmap from the selected hero image
+
+                    val imageUrl = "$BASE_URL${hero.image}"
+                    val bitMap = PaletteGenerator.convertImageUrlToBitmap(imageUrl, context)
+
+                    //check if bitmap is null before extracting colors from it
+
+                    if (bitMap != null) {
+                        viewModel.setPalette(
+                                PaletteGenerator.extractColorsFromBitmap(bitmap = bitMap)
+                        )
+                    }
+                }
+            }
+        }
+
+    })
 
 }
