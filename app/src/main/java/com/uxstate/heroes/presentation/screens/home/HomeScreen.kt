d2d325f93521447221aff46092f9f3d3a8168c69
Tonnie <vontonnie@gmail.com>
2022-10-04 08:53:15 +0300

    Change status bar color on the HomeScreen composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/home/HomeScreen.kt
@@ -3,23 +3,28 @@ package com.uxstate.heroes.presentation.screens.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.presentation.common.ListContent
 import com.uxstate.heroes.presentation.screens.destinations.Destination
 import com.uxstate.heroes.presentation.screens.destinations.SearchScreenDestination
 import com.uxstate.heroes.presentation.screens.home.components.HomeTopBar
+import com.uxstate.heroes.presentation.ui.theme.statusBarColor
 import timber.log.Timber
 
 @com.ramcosta.composedestinations.annotation.Destination
 @Composable
 fun HomeScreen( viewModel: HomeViewModel = hiltViewModel(), navigator: DestinationsNavigator) {
 
+    val uiController = rememberSystemUiController()
+    uiController.setStatusBarColor(color = MaterialTheme.colors.statusBarColor)
 
     val allHeroes = viewModel.getAllHeroes.collectAsLazyPagingItems()
 
@@ -28,7 +33,9 @@ fun HomeScreen( viewModel: HomeViewModel = hiltViewModel(), navigator: Destinati
 
             content = { paddingValues ->
 
-        ListContent(heroes = allHeroes, navigator =navigator, modifier = Modifier.fillMaxSize().padding(paddingValues))
+        ListContent(heroes = allHeroes, navigator =navigator, modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues))
     })
 
 
