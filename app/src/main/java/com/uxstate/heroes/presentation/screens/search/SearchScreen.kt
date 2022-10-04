38e5e028ce717cf7ad17a153584fff864c5f2a9b
Tonnie <vontonnie@gmail.com>
2022-10-04 08:57:25 +0300

    Change status bar color on the SearchScreen composable

--- app/src/main/java/com/uxstate/heroes/presentation/screens/search/SearchScreen.kt
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/search/SearchScreen.kt
@@ -1,15 +1,18 @@
 package com.uxstate.heroes.presentation.screens.search
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.heroes.presentation.common.ListContent
 import com.uxstate.heroes.presentation.screens.search.components.SearchWidget
+import com.uxstate.heroes.presentation.ui.theme.statusBarColor
 import timber.log.Timber
 
 @Destination
@@ -18,6 +21,9 @@ fun SearchScreen(
     navigator: DestinationsNavigator,
     viewModel: SearchViewModel = hiltViewModel()
 ) {
+
+    val uiController = rememberSystemUiController()
+    uiController.setStatusBarColor(color = MaterialTheme.colors.statusBarColor)
 Timber.i("Searxche screen called")
     val query = viewModel.searchQuery
 
