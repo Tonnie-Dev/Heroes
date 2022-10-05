25b7d1ac125afe6caf5958a88e51e1e164921a90
Tonnie <vontonnie@gmail.com>
2022-10-05 19:41:33 +0300

    Add findHero() function to FakeHeroAPI class

--- app/src/test/java/com/uxstate/heroes/data/remote/FakeHeroAPI.kt
+++ app/src/test/java/com/uxstate/heroes/data/remote/FakeHeroAPI.kt
@@ -2,22 +2,24 @@ package com.uxstate.heroes.data.remote
 
 import com.uxstate.heroes.data.remote.dto.ApiResponseDTO
 import com.uxstate.heroes.domain.model.Hero
-/*This class will fake the result from the server*/
-class FakeHeroAPI: HeroAPI {
 
-    private val heroes = listOf<Hero>(Hero(
-            id = 1,
-            name = "Sasuke",
-            image = "",
-            about = "",
-            rating = 4.1,
-            power = 0,
-            month = "",
-            day = "",
-            family = listOf(),
-            abilities = listOf(),
-            natureTypes = listOf()
-    ),
+/*This class will fake the result from the server*/
+class FakeHeroAPI : HeroAPI {
+
+    private val heroes = listOf<Hero>(
+            Hero(
+                    id = 1,
+                    name = "Sasuke",
+                    image = "",
+                    about = "",
+                    rating = 4.1,
+                    power = 0,
+                    month = "",
+                    day = "",
+                    family = listOf(),
+                    abilities = listOf(),
+                    natureTypes = listOf()
+            ),
             Hero(
                     id = 2,
                     name = "Naruto",
@@ -46,7 +48,8 @@ class FakeHeroAPI: HeroAPI {
                     natureTypes = listOf()
             )
 
-            )
+    )
+
     override suspend fun getAllHeroes(page: Int): ApiResponseDTO {
 
         //we will not implement this so we return a blank API
@@ -56,4 +59,23 @@ class FakeHeroAPI: HeroAPI {
     override suspend fun searchHeroes(name: String): ApiResponseDTO {
         TODO("Not yet implemented")
     }
+
+    private fun findHero(name: String): List<Hero> {
+        val foundHero = mutableListOf<Hero>()
+        return if (name.isNotEmpty()) {
+
+            for (hero in heroes) {
+
+                if (hero.name.contains(name, true)) {
+                    foundHero.add(hero)
+                }
+
+            }
+            foundHero
+        } else emptyList()
+
+
+    }
+
+
 }
