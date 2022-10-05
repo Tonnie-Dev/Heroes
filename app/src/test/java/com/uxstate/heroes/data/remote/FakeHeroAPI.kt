fea5b9474fbcedeb7487892ed9f887ea661585f9
Tonnie <vontonnie@gmail.com>
2022-10-05 19:16:47 +0300

    Override getAllHeroes function for the FakeHeroAPI class

--- app/src/test/java/com/uxstate/heroes/data/remote/FakeHeroAPI.kt
+++ app/src/test/java/com/uxstate/heroes/data/remote/FakeHeroAPI.kt
@@ -1,8 +1,52 @@
 package com.uxstate.heroes.data.remote
 
 import com.uxstate.heroes.data.remote.dto.ApiResponseDTO
-
+import com.uxstate.heroes.domain.model.Hero
+/*This class will fake the result from the server*/
 class FakeHeroAPI: HeroAPI {
+
+    private val heroes = listOf<Hero>(Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "",
+            about = "",
+            rating = 4.1,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+    ),
+            Hero(
+                    id = 2,
+                    name = "Naruto",
+                    image = "",
+                    about = "",
+                    rating = 3.9,
+                    power = 0,
+                    month = "",
+                    day = "",
+                    family = listOf(),
+                    abilities = listOf(),
+                    natureTypes = listOf()
+            ),
+
+            Hero(
+                    id = 3,
+                    name = "Sakura",
+                    image = "",
+                    about = "",
+                    rating = 3.0,
+                    power = 0,
+                    month = "",
+                    day = "",
+                    family = listOf(),
+                    abilities = listOf(),
+                    natureTypes = listOf()
+            )
+
+            )
     override suspend fun getAllHeroes(page: Int): ApiResponseDTO {
 
         //we will not implement this so we return a blank API
