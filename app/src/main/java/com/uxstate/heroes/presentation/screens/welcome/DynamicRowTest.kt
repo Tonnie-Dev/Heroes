e10303d998d8de6cfff8cdd4496d3f2473b8a4d2
Tonnie <vontonnie@gmail.com>
2023-03-18 09:25:35 +0300

    Update Accompanist Lib Version to 0.28.00

---
+++ app/src/main/java/com/uxstate/heroes/presentation/screens/welcome/DynamicRowTest.kt
@@ -0,0 +1,54 @@
+package com.uxstate.heroes.presentation.screens.welcome
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun DynamicRowTest() {
+    Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                   // .wrapContentSize()
+                    .background(LightGray)
+    ) {
+        Button(
+                modifier = Modifier
+                        //.defaultMinSize(minWidth = 1.dp, minHeight = 1.dp)
+                ,
+                onClick = { /*TODO*/ },
+              //  contentPadding = PaddingValues(0.dp)
+        ) {
+            Text(
+                    text = "HELLO",
+            )
+        }
+        Button(
+                modifier = Modifier
+                    //    .defaultMinSize(minWidth = 1.dp, minHeight = 1.dp)
+                ,
+                onClick = { /*TODO*/ },
+                //contentPadding = PaddingValues(0.dp)
+        ) {
+            Text(
+                    text = "HELLO",
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DynamicRowTestPreview() {
+
+    DynamicRowTest()
+}
