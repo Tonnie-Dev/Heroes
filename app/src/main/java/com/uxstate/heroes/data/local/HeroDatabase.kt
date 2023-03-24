96196e7a89b2fd01e491855cdfb5f4016d30e2bd
Tonnie <vontonnie@gmail.com>
2023-03-24 08:28:31 +0300

    Add companion object to HeroDatabase

--- app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
+++ app/src/main/java/com/uxstate/heroes/data/local/HeroDatabase.kt
@@ -1,6 +1,8 @@
 package com.uxstate.heroes.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.uxstate.heroes.data.local.dao.HeroDao
@@ -10,8 +12,23 @@ import com.uxstate.heroes.data.local.entity.HeroRemoteKeysEntity
 
 @Database(entities = [HeroEntity::class, HeroRemoteKeysEntity::class], version = 1)
 @TypeConverters(DatabaseConverter::class)
-abstract class HeroDatabase:RoomDatabase() {
+abstract class HeroDatabase : RoomDatabase() {
+
+    companion object {
+
+        fun create(context: Context, useInMemory: Boolean): HeroDatabase {
+
+            val databaseBuilder = if (useInMemory) {
+                Room.inMemoryDatabaseBuilder(context, HeroDatabase::class.java)
+            } else {
+                Room.databaseBuilder(context, HeroDatabase::class.java, "test_database.db")
+            }
+            
+            return databaseBuilder.fallbackToDestructiveMigration()
+                    .build()
+        }
+    }
 
     abstract val heroDao: HeroDao
-    abstract val heroRemoteKeysDao:HeroRemoteKeysDao
+    abstract val heroRemoteKeysDao: HeroRemoteKeysDao
 }
