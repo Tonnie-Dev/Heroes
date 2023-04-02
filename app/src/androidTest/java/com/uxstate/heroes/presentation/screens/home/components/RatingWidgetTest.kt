f116d30d1e210f09ce43d84561fc9fc9df1fc50a
Tonnie <vontonnie@gmail.com>
2023-04-02 09:06:52 +0300

    Add RatingWidgetTest class

---
+++ app/src/androidTest/java/com/uxstate/heroes/presentation/screens/home/components/RatingWidgetTest.kt
@@ -0,0 +1,10 @@
+package com.uxstate.heroes.presentation.screens.home.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class RatingWidgetTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+}
